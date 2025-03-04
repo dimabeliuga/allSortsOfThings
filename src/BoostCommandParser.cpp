5bda9264580a8829e0f45f5026190e1f2ea5ed7a
Liloid Windows <dimabeliuga32@gmail.com>
2025-03-04 09:35:50 +0100

    more in-depth study boost progrma_options

--- src/BoostCommandParser.cpp
+++ src/BoostCommandParser.cpp
@@ -13,21 +13,61 @@
 
 namespace po = boost::program_options;
 
-int main(int argc, char** argv){
+struct Receipt{
+    std::string clientName;
+    int32_t numberOfApples;
+    int32_t numberOfOranges;
+    int32_t numberOfBannanas;
+
+    void printReceipt(){
+        std::cout << "Client: " << clientName << "\n";
+        std::cout << "Apples: " << numberOfApples << "\n";
+        std::cout << "Oranges: " << numberOfOranges << "\n";
+        std::cout << "Bannanas: " << numberOfBannanas << "\n";
+    }
+};
+
+int main(int argc, char* argv[]){
+    Receipt firstClient;
+
+    // description of all options
     po::options_description desc("All options");
+    
+    //*adding options to the description
     desc.add_options()
-        ("apples", po::value<int>(), "how many apples do you have")
-        ("oranges", po::value<int>(), "how many oranges do you have")
+        ("apples", po::value<std::int32_t>(&firstClient.numberOfApples), "how many apples do you have")
+        ("oranges", po::value<std::int32_t>(&firstClient.numberOfOranges), "how many oranges do you have")
+        ("bannanas", po::value<std::int32_t>(&firstClient.numberOfBannanas), "How many apples do you have")
+        ("user name", po::value<std::string>(&firstClient.clientName)->required(), "Your name")
         ("help", "help message")
     ;
+
+    // valuable map to store the values of the options
     po::variables_map vm;
+    // parsing the command line
     po::store(po::parse_command_line(argc, argv, desc), vm);
+    // notifying the map that the parsing is done
     po::notify(vm);
+    
+    if(vm.empty()){
+        std::cout << "No options provided\n";
+        return EXIT_FAILURE;
+    }
+
     if(vm.count("help")){
         std::cout << desc << "\n";
         return EXIT_FAILURE;
     }
-    std::cout << "Fruit count: " << vm["apples"].as<int>() + vm["oranges"].as<int>() << "\n";
-
+   
+    //* you can do like that only if all the options have the samy type
+/*     for(auto& it : vm){
+        std::cout << it.first << " ";
+        if(it.second.empty()){
+            std::cout << "empty\n";
+        } else {
+            std::cout << it.second.as<std::string>() << "\n";
+        }
+    }
+ */
     return 0;
 }
